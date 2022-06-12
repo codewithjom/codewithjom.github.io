b767280159d59b06445cb03707fd6987458f5a9a
Jom Dollesin <codewithjom@gmail.com>
2022-06-12 12:39:34 +0800

    Initial commit.

--- build-site.el
+++ build-site.el
@@ -20,7 +20,7 @@
 (setq org-html-validation-link nil            ;; Don't show validation link
       org-html-head-include-scripts nil       ;; Use our own scripts
       org-html-head-include-default-style nil ;; Use our own styles
-      org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />")
+      org-html-head "<link rel=\"stylesheet\" href=\"https://fniessen.github.io/org-html-themes/org/theme-readtheorg.setup\" />")
 
 ;; Define the publishing project
 (setq org-publish-project-alist
